822ce620a69d1c8c8d6898846c722359a32161c8
johncrawley <johncrawley1@gmail.com>
2021-08-30 12:31:59 +0100

    Initial commit.

---
+++ src/main/java/com/jcrawley/reactiverabbitmqstockservice/UdemyReactiveSpringRabbitmqStockServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jcrawley.reactiverabbitmqstockservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyReactiveSpringRabbitmqStockServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyReactiveSpringRabbitmqStockServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jcrawley/reactiverabbitmqstockservice/UdemyReactiveSpringRabbitmqStockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcrawley.reactiverabbitmqstockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyReactiveSpringRabbitmqStockServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
